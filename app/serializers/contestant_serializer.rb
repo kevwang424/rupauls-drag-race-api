848f4dbc5af4d56d26019f16b15ba935de9a91fc
Kevin Wang <kev.wang424@gmail.com>
2017-05-10 18:02:59 -0400

    added ID into API and made delete action

--- app/serializers/contestant_serializer.rb
+++ app/serializers/contestant_serializer.rb
@@ -1,3 +1,3 @@
 class ContestantSerializer < ActiveModel::Serializer
-  attributes :name, :birth_name, :hometown, :date_of_birth, :date_of_death
+  attributes :id, :name, :birth_name, :hometown, :date_of_birth, :date_of_death
 end
