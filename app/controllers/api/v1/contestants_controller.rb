d7988c6e2d1cb09deddc93a971b0fc87dd8c7066
Kevin Wang <kev.wang424@gmail.com>
2017-05-12 10:14:15 -0400

    added the update function

--- app/controllers/api/v1/contestants_controller.rb
+++ app/controllers/api/v1/contestants_controller.rb
@@ -26,6 +26,13 @@ module Api
         render json: @contestants
       end
 
+      def update
+        @contestant = Contestant.find(params[:id])
+        @contestant.update(contestant_params)
+        @contestants = Contestant.all
+        render json: @contestants
+      end
+
       private
       def contestant_params
         params.require(:contestant).permit(:name, :birth_name, :hometown, :date_of_birth, :date_of_death)
